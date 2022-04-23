328cd1f5bb7c23300f78fbd1dfab96bf8cc0c166
Muhammad Azmi <92970030+MuhAzmii@users.noreply.github.com>
2022-04-23 23:52:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+    include_once('header.php');
+    include_once('sidebar.php');
+?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+			<!-- Content Header (Page header) -->
+			<section class="content-header">
+				<div class="container-fluid">
+					<div class="row mb-2">
+						<div class="col-sm-6">
+							<h1>Info Mahasiswa</h1>
+						</div>
+						<div class="col-sm-6">
+							<ol class="breadcrumb float-sm-right">
+								<li class="breadcrumb-item"><a href="#">Home</a></li>
+								<li class="breadcrumb-item"><a href="#">Welcome</a></li>
+							</ol>
+						</div>
+					</div>
+				</div>
+                <!-- /.container-fluid -->
+			</section>
+
+			<!-- Main content -->
+			<section class="content">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-12">
+							<!-- Default box -->
+							<div class="card">
+								<div class="card-header bg-info color-palette">
+									<h3 class="card-title">Project 1 Penweb 2</h3>
+									<div class="card-tools">
+										<button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+											<i class="fas fa-minus"></i>
+										</button>
+										<button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+											<i class="fas fa-times"></i>
+										</button>
+									</div>
+								</div>
+								<div class="card-body">
+									Nama: Muhammad Azmi <br>
+									NIM: 0110221194 <br>
+									Rombel: TI08 <br>
+									Program Studi : Informatika
+									Semester : 2021/2022 Genap
+								</div>
+								<!-- /.card-body -->
+							</div>
+						<!-- /.card -->
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- /.content -->
+		</div>
+		<!-- /.content-wrapper -->
+
+<?php
+    include_once('footer.php');
+?>
---
+++ sidebar.php
@@ -0,0 +1,186 @@
+		<!-- Main Sidebar Container -->
+		<aside class="main-sidebar sidebar-dark-primary elevation-4">
+			<!-- Brand Logo -->
+			<a href="index.php" class="brand-link">
+				<img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+				<span class="brand-text font-weight-light">Project 1</span>
+			</a>
+
+			<!-- Sidebar -->
+			<div class="sidebar">
+				<!-- Sidebar user (optional) -->
+				<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+					<div class="image">
+						<img src="https://avatars.githubusercontent.com/u/92970030?s=400&u=6433b08d59afbe4b6e4b7e95e6262ad4fa141d06&v=4" class="img-circle elevation-2" alt="User Image">
+					</div>
+					<div class="info">
+						<a href="#" class="d-block">Muhammad Azmi</a>
+					</div>
+				</div>
+
+				<!-- SidebarSearch Form -->
+				<div class="form-inline">
+					<div class="input-group" data-widget="sidebar-search">
+						<input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+						<div class="input-group-append">
+							<button class="btn btn-sidebar">
+								<i class="fas fa-search fa-fw"></i>
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<!-- Sidebar Menu -->
+				<nav class="mt-2">
+					<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+						<!-- Add icons to the links using the .nav-icon class
+						with font-awesome or any other icon font library -->
+						<li class="nav-item menu-open">
+							<a href="#" class="nav-link active">
+								<i class="nav-icon fas fa-tachometer-alt"></i>
+								<p>
+									Praktikum Penweb 
+								</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="nav-icon fas fa-tree"></i>
+								<p>
+									Praktikum 1 
+									<i class="fas fa-angle-left right"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="p1/array/array_siswa.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Array Siswa</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="p1/01_info.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Info</p>
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="nav-icon fas fa-tree"></i>
+								<p>
+									Praktikum 2 
+									<i class="fas fa-angle-left right"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="p2/Form Belanja.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Form Nilai</p>
+										<!-- if else, grade, dan keterangan lulus -->
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="p2-form_nilai.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Form Nilai</p>
+										<!-- switch case -->
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="nav-icon fas fa-tree"></i>
+								<p>
+									Praktikum 3 
+									<i class="fas fa-angle-left right"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="p3/web dinamis/index.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Web Dinamis</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="p3/form nilai/form nilai.html" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Form Nilai Function</p>
+										<!-- include_once dengan libfunction -->
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="nav-icon fas fa-tree"></i>
+								<p>
+									Praktikum 4 
+									<i class="fas fa-angle-left right"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="p4/daftar_mahasiswa.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Daftar Mahasiswa</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="p4/nilai_Mahasiswa.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p> Nilai Mahasiswa</p>
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="nav-icon fas fa-tree"></i>
+								<p>
+									Praktikum 5 
+									<i class="fas fa-angle-left right"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="p5/fruit.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Fruit</p>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a href="p5/dispenser.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Dispenser</p>
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="nav-icon fas fa-tree"></i>
+								<p>
+									Praktikum 6 - UTS
+									<i class="fas fa-angle-left right"></i>
+								</p>
+							</a>
+							<ul class="nav nav-treeview">
+								<li class="nav-item">
+									<a href="project/index.php" class="nav-link">
+										<i class="far fa-circle nav-icon"></i>
+										<p>Form BMI Pasien</p>
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+				<!-- /.sidebar-menu -->
+			</div>
+		<!-- /.sidebar -->
+		</aside>
